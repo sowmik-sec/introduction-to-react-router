ba0f390f3d1a39cae0532370da480c2e3d0734ea
sowmik-sec <sowmik.sec@gmail.com>
2024-06-28 11:58:12 +0600

    Active Route, Loading Spinner, UseLocation, UseNavigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 function Header() {
   return (
     <div>
       <h2>Navbar</h2>
       <nav>
-        <Link to={"/"}>Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact us</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/posts">Posts</Link>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact us</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
       </nav>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 function Home() {
+  const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
       <Header />
       <h2>This is home component</h2>
-      <Outlet />
+      {navigation.state === "loading" ? <p>Loading...</p> : <Outlet />}
       <Footer />
     </div>
   );
